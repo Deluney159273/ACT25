5589b20d50d49b80a61b88f99afa8930bad299d4
Deluney159273 <da.pascal@duocuc.cl>
2026-06-23 14:09:27 -0400

    commit_1_agregar
    Desarrollo de la actividad, con el menu y la opcion uno de agregar. Ademas, tenemos un metodo de buscar con patente, para encontrar la posicion del vehiculo en la lista

---
+++ funciones.py
@@ -0,0 +1,39 @@
+
+vehiculos=[]
+
+def buscar(patente):
+    for i in range(len(vehiculos)):
+        if vehiculos[i]["patente"]==patente:
+            return i
+    return -1
+
+def agregar(patente,tipo,anio,precio):
+    if len(patente)!=6:
+        print("Numero de cartacteres no valido")
+        return
+    elif" " in patente:
+        print("No puede tener espacios en blanco")
+        return
+    elif buscar(patente)>=0:
+        print("Patente repetida")
+        return
+    elif tipo.lower() not in ("sedan","suv","camioneta"):
+        print("Tipo no valido")
+        return
+    elif anio<2015 or anio>2026:
+        print("Año no valido")
+        return
+    elif precio<=5000000:
+        print("Precio no valido")
+        return
+    
+    auto={
+        "patente":patente,
+        "tipo":tipo,
+        "anio":anio,
+        "precio":precio
+    }
+    vehiculos.append(auto)
+    print("Vehiculo Registrado")
+
+
