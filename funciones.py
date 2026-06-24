9f49fb36f34f80517cd4a36836f8d0ee94978b3b
Deluney159273 <da.pascal@duocuc.cl>
2026-06-23 22:57:07 -0400

    commit_4_ActualizarYDescuento
    Agregué la opción 3 de de actualizar el precio y año, además, agregue la opción 4, la cual es aplicar un descuento masivo por año

--- funciones.py
+++ funciones.py
@@ -50,3 +50,36 @@ def listarConIVA():
             print(f"{i+1} {vehiculos[i]["patente"]:<8} {vehiculos[i]["tipo"]:<10} {vehiculos[i]["anio"]:<6} {vehiculos[i]["precio"]:<10}")
     else:
         print("No hay vehiculos registrados")
+
+def actualizar(patente):
+    encontrado=buscar(patente)
+    if encontrado>=0:
+        anio=int(input("Ingrese el año: "))
+        while anio<2015 or anio>2026:
+            print("Año no valido, debe ser entre 2015 y 2026")
+            anio=int(input("Ingrese el año: "))
+        precio=int(input("Ingrese el precio: "))
+        while precio<=5000000:
+            print("Precio no valido, debe ser mayor a 5 millones")
+            precio=int(input("Ingrese el precio: "))
+        vehiculos[encontrado]["anio"]=anio
+        vehiculos[encontrado]["precio"]=precio
+        print("Datos actualizados")
+    else:
+        print("Error: El vehículo con la patente proporcionada no está registrado")
+
+def descuento(anio,porcentaje):
+    if anio<2015 or anio>2026:
+        print("Año no valido")
+        return
+    else:
+        if porcentaje>0 and porcentaje<=100:
+            for i in range(len(vehiculos)):
+                if vehiculos[i]["anio"]==anio:
+                    precio_actual=vehiculos[i]["precio"]
+                    descuento_aplicado=precio_actual*(porcentaje/100)
+                    vehiculos[i]["precio"]=int(precio_actual-descuento_aplicado)
+                    print(f"Descuento del {porcentaje}% aplicado a los vehiculos del año {anio}")
+        else:
+            print("Porcentaje no valido")
+            return
